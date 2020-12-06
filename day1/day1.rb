20300ece1a25bb2cc92088c71d2df96f4126057d
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-06 22:05:20 +0800

    Separate Part1 code

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,20 +1,22 @@
 require 'set'
 
-def multiply_matching_entries_in_file(path)
-  # REFACTOR: instead of loading the entire file, see if it's possible to stream somehow? Since the approach seems to fine making a single pass
-  expanded_path = File.expand_path(path, File.expand_path(File.dirname(__FILE__)))
-  input = File.readlines(expanded_path).map(&:to_i)
-  multiply_matching_entries(input)
-end
+module Part1
+  def self.multiply_matching_entries_in_file(path)
+    # REFACTOR: instead of loading the entire file, see if it's possible to stream somehow? Since the approach seems to fine making a single pass
+    expanded_path = File.expand_path(path, File.expand_path(File.dirname(__FILE__)))
+    input = File.readlines(expanded_path).map(&:to_i)
+    multiply_matching_entries(input)
+  end
 
-def multiply_matching_entries(input)
-  possible_matches = Set.new
+  def self.multiply_matching_entries(input)
+    possible_matches = Set.new
 
-  input.each do |i|
-    if possible_matches.include?(i)
-      return (2020 - i) * i
-    else
-      possible_matches.add(2020 - i)
+    input.each do |i|
+      if possible_matches.include?(i)
+        return (2020 - i) * i
+      else
+        possible_matches.add(2020 - i)
+      end
     end
   end
 end
--- day1/day1_spec.rb
+++ day1/day1_spec.rb
@@ -1,40 +1,42 @@
 require 'rspec'
 require_relative './day1.rb'
 
-RSpec.describe '#multiply_matching_entries' do
-  it 'multiples entries when both add to 2020' do
-    input = [
-      2019,
-      1
-    ]
-    expect(multiply_matching_entries(input)).to eq(2019)
+RSpec.describe 'part 1' do
+  describe '#multiply_matching_entries' do
+    it 'multiples entries when both add to 2020' do
+      input = [
+        2019,
+        1
+      ]
+      expect(Part1.multiply_matching_entries(input)).to eq(2019)
+    end
+
+    it 'multiplies the correct entries when there are others' do
+      input = [
+        2019,
+        2,
+        1,
+        3
+      ]
+      expect(Part1.multiply_matching_entries(input)).to eq(2019)
+    end
+
+    it 'multiplies the correct entries' do
+      input = [
+        1721,
+        979,
+        366,
+        299,
+        675,
+        1456
+      ]
+      expect(Part1.multiply_matching_entries(input)).to eq(514579)
+    end
   end
 
-  it 'multiplies the correct entries when there are others' do
-    input = [
-      2019,
-      2,
-      1,
-      3
-    ]
-    expect(multiply_matching_entries(input)).to eq(2019)
-  end
-
-  it 'multiplies the correct entries' do
-    input = [
-      1721,
-      979,
-      366,
-      299,
-      675,
-      1456
-    ]
-    expect(multiply_matching_entries(input)).to eq(514579)
-  end
-end
-
-RSpec.describe '#multiply_matching_entries_in_file' do
-  it 'multiplies the correct entries' do
-    expect(multiply_matching_entries_in_file('example.txt')).to eq(514579)
+  describe '#multiply_matching_entries_in_file' do
+    it 'multiplies the correct entries' do
+      expect(Part1.multiply_matching_entries_in_file('example.txt')).to eq(514579)
+    end
   end
 end
