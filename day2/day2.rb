08f0e8a249a9eb69970f5bf983b5957d24b4d60a
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-08 17:31:45 +0800

    Refactor to class

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,5 +1,14 @@
-module PartOne
-  def self.check_password(input)
+require_relative '../helpers'
+
+class PartOne
+  include FileHelpers
+
+  # def check_passwords_in_file(path)
+  #   passwords = readlines_from_file(path)
+  #   valid_passwords
+  # end
+
+  def check_password(input)
     policy_str, password = input.split(': ')
     minmax_str, letter = policy_str.split(' ')
     min, max = minmax_str.split('-').map(&:to_i)
--- day2/day2_spec.rb
+++ day2/day2_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe PartOne do
   describe '#check_password' do
     it 'returns true if min bound of policy is hit' do
       input = '1-3 a: abcde'
-      expect(PartOne.check_password(input)).to eq(true)
+      expect(PartOne.new.check_password(input)).to eq(true)
     end
 
     it 'returns false if policy character is not present' do
       input = '1-3 b: cdefg'
-      expect(PartOne.check_password(input)).to eq(false)
+      expect(PartOne.new.check_password(input)).to eq(false)
     end
 
     it 'returns true if max bound of policy is hit' do
       input = '2-9 c: ccccccccc'
-      expect(PartOne.check_password(input)).to eq(true)
+      expect(PartOne.new.check_password(input)).to eq(true)
     end
   end
 end
