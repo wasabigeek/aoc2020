1813ea5846535af325089f987f2564b56fb05b39
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-08 17:15:01 +0800

    Get first case working

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,4 +1,14 @@
 module PartOne
   def self.check_password(input)
+    policy_str, password = input.split(': ')
+    minmax_str, letter = policy_str.split(' ')
+    min, max = minmax_str.split('-').map(&:to_i)
+
+    matches = password.split('').reduce(0) do |count, char|
+      count += 1 if char == letter
+      count
+    end
+
+    matches <= max && matches >= min
   end
 end
