2d4212f2815d8db2eb5ff7ba2dfb96c8f3b718f0
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-08 17:17:02 +0800

    Add more test cases

--- day2/day2_spec.rb
+++ day2/day2_spec.rb
@@ -3,10 +3,18 @@ require_relative './day2.rb'
 
 RSpec.describe PartOne do
   describe '#check_password' do
-    it 'handles the min case' do
+    it 'returns true if min bound of policy is hit' do
       input = '1-3 a: abcde'
-      # 1-3 b: cdefg
-      # 2-9 c: ccccccccc'
+      expect(PartOne.check_password(input)).to eq(true)
+    end
+
+    it 'returns false if policy character is not present' do
+      input = '1-3 b: cdefg'
+      expect(PartOne.check_password(input)).to eq(false)
+    end
+
+    it 'returns true if max bound of policy is hit' do
+      input = '2-9 c: ccccccccc'
       expect(PartOne.check_password(input)).to eq(true)
     end
   end
