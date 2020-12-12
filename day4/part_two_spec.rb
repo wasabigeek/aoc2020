d45f262e1be621def584f96a9b5b2898e6e4c29f
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-12 16:04:24 +0800

    Change parse_passports to also return values

--- day4/part_two_spec.rb
+++ day4/part_two_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Day4::PartTwo do
   end
 
   describe '#parse_passports' do
-    it 'returns fields for each passport' do
+    it 'returns fields and values for each passport' do
       input = <<~TXT
         ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
         byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -41,10 +41,18 @@ RSpec.describe Day4::PartTwo do
       TXT
 
       expect(described_class.new.parse_passports(input)).to eq([
-        %w[ecl pid eyr hcl byr iyr cid hgt],
-        %w[iyr ecl cid eyr pid hcl byr],
-        %w[hcl iyr eyr ecl pid byr hgt],
-        %w[hcl eyr pid iyr ecl hgt]
+        [
+          ["ecl", "gry"], ["pid", "860033327"], ["eyr", "2020"], ["hcl", "#fffffd"], ["byr", "1937"], ["iyr", "2017"], ["cid", "147"], ["hgt", "183cm"]
+        ],
+        [
+          ["iyr", "2013"], ["ecl", "amb"], ["cid", "350"], ["eyr", "2023"], ["pid", "028048884"], ["hcl", "#cfa07d"], ["byr", "1929"]
+        ],
+        [
+          ["hcl", "#ae17e1"], ["iyr", "2013"], ["eyr", "2024"], ["ecl", "brn"], ["pid", "760753108"], ["byr", "1931"], ["hgt", "179cm"]
+        ],
+        [
+          ["hcl", "#cfa07d"], ["eyr", "2025"], ["pid", "166559648"], ["iyr", "2011"], ["ecl", "brn"], ["hgt", "59in"]
+        ]
       ])
     end
   end
