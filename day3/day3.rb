eacdc35838a4d380fa1e72a25ac3569e18732ae3
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-12 14:02:49 +0800

    Copy code for PartTwo

--- day3/day3.rb
+++ day3/day3.rb
@@ -31,3 +31,39 @@ class PartOne
     char == '#'
   end
 end
+
+class PartTwo
+  include FileHelpers
+
+  def count_trees_from_file(path, right, down)
+    count_trees_in_path(readlines_from_file(path))
+  end
+
+  def count_trees_in_path(terrain_template, right, down)
+    return check_tree(0, 0, terrain_template)
+  end
+
+  private
+
+  def check_tree(x, y, terrain_template)
+    max_width = terrain_template.first.size
+    max_height = terrain_template.size
+
+    template_x = (x + 1) % max_width - 1
+    template_x = max_width - 1 if template_x == -1
+
+    tree_count = is_tree?(terrain_template[y][template_x]) ? 1 : 0
+    if y + 1 >= max_height
+      return tree_count
+    end
+
+    return tree_count + check_tree(x + 3, y + 1, terrain_template)
+  end
+
+  def is_tree?(char)
+    char == '#'
+  end
+end
+
+
+# puts PartOne.new.count_trees_from_file('day3/input.txt')
--- day3/day3_spec.rb
+++ day3/day3_spec.rb
@@ -35,3 +35,24 @@ RSpec.describe PartOne do
     end
   end
 end
+
+RSpec.describe PartTwo do
+  describe '#count_trees_in_path' do
+    it 'returns the correct count for original slope' do
+      right, down = 3, 1
+      input = [
+        '....',
+        '...#',
+        '..#.'
+      ]
+      expect(PartTwo.new.count_trees_in_path(input, right, down)).to eq(2)
+    end
+  end
+
+  describe '#count_trees_from_file' do
+    it 'returns the correct count for the original slope' do
+      right, down = 3, 1
+      expect(PartTwo.new.count_trees_from_file('day3/example.txt', right, down)).to eq(7)
+    end
+  end
+end
