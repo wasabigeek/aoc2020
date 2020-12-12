a1b5109f3ce1e32ee4ab4ef368da2cf05a46bb15
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-12 13:40:23 +0800

    Refactor to a more recursive solution

--- day3/day3.rb
+++ day3/day3.rb
@@ -1,22 +1,21 @@
 class PartOne
-  def count_trees_in_path(terrain)
-    max_width = terrain.first.size
-    max_height = terrain.size
-    x, y = 0, 0
+  def count_trees_in_path(terrain_template)
+    return check_tree(0, 0, terrain_template)
+  end
 
-    total_trees = 0
-    while y < (max_height - 1)
-      x += 3
-      x -= max_width if x >= max_width
-      y += 1
-      y -= max_height if y >= max_height
+  def check_tree(x, y, terrain_template)
+    max_width = terrain_template.first.size
+    max_height = terrain_template.size
 
-      if is_tree?(terrain[y][x])
-        total_trees += 1
-      end
+    template_x = (x + 1) % max_width - 1
+    template_x = max_width - 1 if template_x == -1
+
+    tree_count = is_tree?(terrain_template[y][template_x]) ? 1 : 0
+    if y + 1 >= max_height
+      return tree_count
     end
 
-    total_trees
+    return tree_count + check_tree(x + 3, y + 1, terrain_template)
   end
 
   def is_tree?(char)
