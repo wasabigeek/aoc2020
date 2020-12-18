36a1e6ab1ba6da947ebf9a2fbf82a67ddaa3612b
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-18 23:07:41 +0800

    Handle RL partition

--- day5/part_one.rb
+++ day5/part_one.rb
@@ -3,21 +3,36 @@ require_relative '../helpers'
 module Day5
   class PartOne
     attr_accessor :row_upper_bound, :row_lower_bound
+    attr_accessor :col_upper_bound, :col_lower_bound
 
     include FileHelpers
 
     def initialize
       @row_upper_bound = 127
       @row_lower_bound = 0
+      @col_upper_bound = 7
+      @col_lower_bound = 0
     end
 
-    def partition_row(instruction)
-      mid = (row_lower_bound.to_f + row_upper_bound.to_f) / 2
+    def partition_once(instruction)
+      if ['F', 'B'].include? instruction
+        mid = (row_lower_bound.to_f + row_upper_bound.to_f) / 2
 
-      if instruction == 'F'
-        @row_upper_bound = mid.floor
-      elsif instruction == 'B'
-        @row_lower_bound = mid.ceil
+        if instruction == 'F'
+          @row_upper_bound = mid.floor
+        elsif instruction == 'B'
+          @row_lower_bound = mid.ceil
+        end
+      end
+
+      if ['L', 'R'].include? instruction
+        mid = (col_lower_bound.to_f + col_upper_bound.to_f) / 2
+
+        if instruction == 'L'
+          @col_upper_bound = mid.floor
+        elsif instruction == 'R'
+          @col_lower_bound = mid.ceil
+        end
       end
     end
   end
--- day5/part_one_spec.rb
+++ day5/part_one_spec.rb
@@ -2,32 +2,44 @@ require 'rspec'
 require_relative './part_one.rb'
 
 RSpec.describe Day5::PartOne do
-  describe '#partition_row' do
+  describe '#partition_once' do
     it 'sets the correct rows' do
       part_one = described_class.new
       expect(part_one.row_upper_bound).to eq(127)
       expect(part_one.row_lower_bound).to eq(0)
-      part_one.partition_row('F')
+      part_one.partition_once('F')
       expect(part_one.row_upper_bound).to eq(63)
       expect(part_one.row_lower_bound).to eq(0)
-      part_one.partition_row('B')
+      part_one.partition_once('B')
       expect(part_one.row_upper_bound).to eq(63)
       expect(part_one.row_lower_bound).to eq(32)
-      part_one.partition_row('F')
+      part_one.partition_once('F')
       expect(part_one.row_upper_bound).to eq(47)
       expect(part_one.row_lower_bound).to eq(32)
-      part_one.partition_row('B')
+      part_one.partition_once('B')
       expect(part_one.row_upper_bound).to eq(47)
       expect(part_one.row_lower_bound).to eq(40)
-      part_one.partition_row('B')
+      part_one.partition_once('B')
       expect(part_one.row_upper_bound).to eq(47)
       expect(part_one.row_lower_bound).to eq(44)
-      part_one.partition_row('F')
+      part_one.partition_once('F')
       expect(part_one.row_upper_bound).to eq(45)
       expect(part_one.row_lower_bound).to eq(44)
-      part_one.partition_row('F')
+      part_one.partition_once('F')
       expect(part_one.row_upper_bound).to eq(44)
       expect(part_one.row_lower_bound).to eq(44)
+
+      expect(part_one.col_upper_bound).to eq(7)
+      expect(part_one.col_lower_bound).to eq(0)
+      part_one.partition_once('R')
+      expect(part_one.col_upper_bound).to eq(7)
+      expect(part_one.col_lower_bound).to eq(4)
+      part_one.partition_once('L')
+      expect(part_one.col_upper_bound).to eq(5)
+      expect(part_one.col_lower_bound).to eq(4)
+      part_one.partition_once('R')
+      expect(part_one.col_upper_bound).to eq(5)
+      expect(part_one.col_lower_bound).to eq(5)
     end
   end
 end
